0ae04cafdf419cfb5a4f9473dcfd5fa14fa52c97
rotem-ziv21 <162511267+rotem-ziv21@users.noreply.github.com>
2024-11-18 17:35:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // הוספת הכתובת להאזנה לכל הממשקים
+    port: process.env.PORT || 3000, // שימוש בפורט דינמי
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
+  },
+  preview: {
+    host: '0.0.0.0', // הוספת הכתובת להאזנה לכל הממשקים גם עבור תצוגה מקדימה
+    port: process.env.PORT || 3000 // שימוש בפורט דינמי עבור תצוגה מקדימה
   }
 });
